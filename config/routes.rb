c6891b3c18a8cf8a2506dbe2b0d5ba196d058c4c
Kumilachew-g <kumilachew1212@gmail.com>
2022-12-13 16:46:35 +0300

    Add recipe view routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'shopping_list/:recipe_id', to: 'recipes#shopping_list'
+  get 'public_recipes', to: 'recipes#public'
+  get 'recipe_foods/:recipe_id', to: 'recipe_foods#new'
+  post 'recipe_foods/:recipe_id', to: 'recipe_foods#create'
+  resources :recipe_foods, only: [:destroy, :update, :edit]
+  resources :recipes, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :foods, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  root to: "foods#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
