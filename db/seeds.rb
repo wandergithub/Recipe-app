5587a43e8086b21272c6773768e64da21cdfdb22
wandergithub <wandergonzalez0@hotmail.com>
2022-12-13 15:04:07 -0400

    populated seed file to add instances to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+(1..10).each do |id|
+  user = User.create(name: "User #{id}")
+  recipe = Recipe.create(name: "Recipe #{id}", description: "Lorem ipsum dolor saber", user:)
+  food = Food.create(name: "Food #{id}", measurement_unit: "pund", user:)
+end
+  Recipe.first.foods = Food.all
