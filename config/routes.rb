11a32f0319d14c6e7716a731ffaada0bd5de60b5
wandergithub <wandergonzalez0@hotmail.com>
2022-12-13 20:32:43 -0400

    Add routes to create and destroy foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :foods, only: [:index]
+  resources :foods, only: [:index, :create, :destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
