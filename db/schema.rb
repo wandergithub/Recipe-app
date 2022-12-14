39bb0213a1a971ac15269d80cc0952dcbde9de4e
Kumilachew-g <kumilachew1212@gmail.com>
2022-12-15 00:29:06 +0300

    Change from time to float to enhance

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_13_174718) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_14_205716) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,13 +37,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_174718) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.time "preparation_time"
-    t.time "cooking_time"
     t.string "description"
     t.boolean "public"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.float "preparation_time", default: 0.0
+    t.float "cooking_time", default: 0.0
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
@@ -59,7 +59,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_174718) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 
   add_foreign_key "foods", "users", on_delete: :cascade
   add_foreign_key "recipe_foods", "foods", on_delete: :cascade
