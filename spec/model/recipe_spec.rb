466c5c2b989dde2115733a1b952297c4ca53bf17
Kumilachew-g <kumilachew1212@gmail.com>
2022-12-15 22:18:33 +0300

    Fix rubocop

--- spec/model/recipe_spec.rb
+++ spec/model/recipe_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-    let(:recipe) do
-        Recipe.new(name: 'recipe 1', user: User.new(name: 'name', password: 'password123', email: 'hana@gmail.com'))
+  let(:recipe) do
+    Recipe.new(name: 'recipe 1', user: User.new(name: 'name', password: 'password123', email: 'hana@gmail.com'))
+  end
+
+  describe 'Model validation' do
+    it 'Passes validation' do
+      expect(recipe).to be_valid
     end
 
-    describe 'Model validation' do
-        it 'Passes validation' do
-            expect(recipe).to be_valid
-        end
-
-        it 'Validates :name presence' do
-            recipe.name = nil
-            expect(recipe).to_not be_valid
-        end
-        end
+    it 'Validates :name presence' do
+      recipe.name = nil
+      expect(recipe).to_not be_valid
+    end
+  end
 
   it 'Validates :user presence' do
     recipe.user = nil
