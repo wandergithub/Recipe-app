fb3dabf29238a4132085168a0a6ed039fedb0956
wandergithub <wandergonzalez0@hotmail.com>
2022-12-15 11:35:17 -0400

    write Food model validation specs

---
+++ spec/model/food_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Food, type: :model do
+  let(:food) { Food.new(name: "food 1", user: User.new(name: "name", password: "password123", email: "sample@exmaple.com")) }
+  
+  describe 'Model validation' do
+    it "Passes validation" do
+      expect(food).to be_valid
+    end
+
+    it "Validates :name presence" do
+      food.name = nil
+      expect(food).to_not be_valid
+    end
+  end
+end
