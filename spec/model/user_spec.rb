fe96052269eb79c06a4c3173bdce03b4bb3b3529
wandergithub <wandergonzalez0@hotmail.com>
2022-12-15 11:25:06 -0400

    write user model specs

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,15 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'validation' do
-    let(:user) { User.new(name: "name 1", email: "example@hotmail.com", password: "example_password") }
-
+  let(:user) { User.new(name: "name 1", email: "example@hotmail.com", password: "example_password") }
+  
+  describe 'Model validation' do
     it "Passes validation" do
       expect(user).to be_valid
     end
 
     it "Validates :name presence" do
       user.name = nil
+      expect(user).to_not be_valid
+    end
+  end
+  
+  describe "User authentication validation" do
+    it "Validates :email presence" do
+      user.email = nil
+      expect(user).to_not be_valid
+    end
+
+    it "Validates :password presence" do
+      user.password = nil
       expect(user).to_not be_valid
     end
   end
