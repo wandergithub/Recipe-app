c2a617947e4a79a70049820d827ca6d26d3419d8
wandergithub <wandergonzalez0@hotmail.com>
2022-12-15 12:46:56 -0400

    rubocop

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 describe 'Show all foods of current user', type: :feature do
   before(:each) do
     # Log in
-    @user = User.create(id: 1, name: "example", email: "example@gmail.com", password: "1234567")
+    @user = User.create(id: 1, name: 'example', email: 'example@gmail.com', password: '1234567')
     visit new_user_session_path
     fill_in 'user_email', with: @user.email
     fill_in 'user_password', with: @user.password
     click_button 'Log in'
     # end
-    @food = Food.create(user: @user, name: "food 1", measurement_unit: 'grams', price: 12, quantity: 3)
+    @food = Food.create(user: @user, name: 'food 1', measurement_unit: 'grams', price: 12, quantity: 3)
   end
   context 'visit /foods' do
     before(:example) do
@@ -17,9 +17,9 @@ describe 'Show all foods of current user', type: :feature do
     end
 
     it 'Can see foods table' do
-      expect(page).to have_content("Food")
-      expect(page).to have_content("Quantity")
-      expect(page).to have_content("Measurement unit")
+      expect(page).to have_content('Food')
+      expect(page).to have_content('Quantity')
+      expect(page).to have_content('Measurement unit')
     end
 
     it 'Can see a User created food' do
