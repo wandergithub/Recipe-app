e9d232875f6d165595cf99ff9bc4b725f5414d2c
wandergithub <wandergonzalez0@hotmail.com>
2022-12-15 12:46:13 -0400

    Add integration specs to /foods feature

--- spec/model/food_spec.rb
+++ spec/model/food_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  let(:food) { Food.new(name: "food 1", user: User.new(name: "name", password: "password123", email: "sample@exmaple.com")) }
-  
+  let(:food) do
+    Food.new(name: 'food 1', user: User.new(name: 'name', password: 'password123', email: 'sample@exmaple.com'))
+  end
+
   describe 'Model validation' do
-    it "Passes validation" do
+    it 'Passes validation' do
       expect(food).to be_valid
     end
 
-    it "Validates :name presence" do
+    it 'Validates :name presence' do
       food.name = nil
       expect(food).to_not be_valid
     end
--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.new(name: "name 1", email: "example@hotmail.com", password: "example_password") }
-  
+  let(:user) { User.new(name: 'name 1', email: 'example@hotmail.com', password: 'example_password') }
+
   describe 'Model validation' do
-    it "Passes validation" do
+    it 'Passes validation' do
       expect(user).to be_valid
     end
 
-    it "Validates :name presence" do
+    it 'Validates :name presence' do
       user.name = nil
       expect(user).to_not be_valid
     end
   end
-  
-  describe "User authentication validation" do
-    it "Validates :email presence" do
+
+  describe 'User authentication validation' do
+    it 'Validates :email presence' do
       user.email = nil
       expect(user).to_not be_valid
     end
 
-    it "Validates :password presence" do
+    it 'Validates :password presence' do
       user.password = nil
       expect(user).to_not be_valid
     end
